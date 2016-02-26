93c51591954073a6d6212943ccf891cd5c573f66
Sergio Mota <samota@uppenjamo.edu.mx>
2016-02-25 19:46:32 -0600

    Added method to create taught subjects from professor planning

--- coordination/scripts/populate/old/db_interface.py
+++ coordination/scripts/populate/old/db_interface.py
@@ -1,8 +1,9 @@
 import cast
 from coordination.models import (
     Classroom,
+    Professor,
     Student,
-#     StudentInClassroom,
+    TaughtSubject,
 )
 import xls_interface
 
@@ -14,8 +15,8 @@ def create_classroom(
         degree,
         file_type,
         identifier,
-        leader,
-        quarter
+        quarter,
+        leader=None
 ):
     tutor_name = xls_interface.read_tutor_name(career, degree, file_type, identifier, quarter)
     professor = model_interface.get_professor(tutor_name)
@@ -50,8 +51,45 @@ def create_group_from_academic_load(
     else:
         students = find_students(file_type, quarter)
 
-    classroom = create_classroom(career, degree, file_type, identifier, students[0], quarter)
+    classroom = create_classroom(career, degree, file_type, identifier, quarter, None)
     register_students_in_classroom(classroom, students)
+
+
+def create_taught_subjects_from_professor_planning(
+        career,
+        quarter
+):
+    file_type = cast.FileType.professor_planning
+    taught_subjects_data = xls_interface.read_taught_subjects(career=career, file_type=file_type, quarter=quarter)
+
+    for professor in Professor.objects.all():
+        professor.active = False
+        professor.save()
+
+    taught_subjects = []
+    try:
+        for entry in taught_subjects_data:
+            professor = entry['professor']
+            professor.active = True
+
+            classroom = model_interface.get_classroom(
+                career=career,
+                degree=entry['classroom_degree'],
+                identifier=entry['classroom_identifier'],
+                quarter=quarter
+            )
+
+            taught_subject = TaughtSubject.objects.create(
+                classroom=classroom,
+                professor=professor,
+                subject=entry['subject']
+            )
+
+            taught_subjects.append(taught_subject)
+    except:
+        raise
+
+    return taught_subjects
 
 
 def create_students(
