d89e4d4e463a832a8bf377e609f1436077f762d2
Sergio Mota <samota@uppenjamo.edu.mx>
2016-02-29 11:57:12 -0600

    Added view for ISW homepage

--- isw/urls.py
+++ isw/urls.py
@@ -22,6 +22,7 @@ import coordination.views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^isw/$', TemplateView.as_view(template_name='isw.html'), name='isw'),	
     url(r'^isw/calls/logo/$', TemplateView.as_view(template_name='isw/calls/logo.html'), name='isw-calls-logo'),
 
     url(r'^sicpe/delete/career/(?P<pk>\d+)/$', coordination.views.DeleteCareerView.as_view(), name='careers-delete'),
@@ -29,28 +30,33 @@ urlpatterns = [
     url(r'^sicpe/delete/generation/(?P<pk>\d+)/$', coordination.views.DeleteGenerationView.as_view(), name='generations-delete'),
     url(r'^sicpe/delete/professor/(?P<pk>\d+)/$', coordination.views.DeleteProfessorView.as_view(), name='professors-delete'),
     url(r'^sicpe/delete/student/(?P<pk>\d+)/$', coordination.views.DeleteStudentView.as_view(), name='students-delete'),
+    url(r'^sicpe/delete/student-in-taught-subject/(?P<pk>\d+)/$', coordination.views.DeleteStudentInTaughtSubjectView.as_view(), name='student-in-taught-subjects-delete'),
 
     url(r'^sicpe/detail/career/(?P<pk>\d+)/$', coordination.views.CareerView.as_view(), name='careers-view',),
     url(r'^sicpe/detail/classroom/(?P<pk>\d+)/$', coordination.views.ClassroomView.as_view(), name='classrooms-view',),
     url(r'^sicpe/detail/generation/(?P<pk>\d+)/$', coordination.views.GenerationView.as_view(), name='generations-view',),
     url(r'^sicpe/detail/professor/(?P<pk>\d+)/$', coordination.views.ProfessorView.as_view(), name='professors-view',),
     url(r'^sicpe/detail/student/(?P<pk>\d+)/$', coordination.views.StudentView.as_view(), name='students-view',),
+    url(r'^sicpe/detail/student-in-taught-subject/(?P<pk>\d+)/$', coordination.views.StudentInTaughtSubjectView.as_view(), name='student-in-taught-subjects-view',),
 
     url(r'^sicpe/new/career/$', coordination.views.CreateCareerView.as_view(), name='careers-new',),
     url(r'^sicpe/new/classroom/$', coordination.views.CreateClassroomView.as_view(), name='classrooms-new',),
     url(r'^sicpe/new/generation/$', coordination.views.CreateGenerationView.as_view(), name='generations-new',),
     url(r'^sicpe/new/professor/$', coordination.views.CreateProfessorView.as_view(), name='professors-new',),
     url(r'^sicpe/new/student/$', coordination.views.CreateStudentView.as_view(), name='students-new',),
+    url(r'^sicpe/new/student-in-taught-subject/$', coordination.views.CreateStudentInTaughtSubjectView.as_view(), name='student-in-taught-subjects-new',),
 
     url(r'^sicpe/list/career/$', coordination.views.ListCareerView.as_view(), name='careers-list',),
     url(r'^sicpe/list/classroom/$', coordination.views.ListClassroomView.as_view(), name='classrooms-list',),
     url(r'^sicpe/list/generation/$', coordination.views.ListGenerationView.as_view(), name='generations-list',),
     url(r'^sicpe/list/professor/$', coordination.views.ListProfessorView.as_view(), name='professors-list',),
     url(r'^sicpe/list/student/$', coordination.views.ListStudentView.as_view(), name='students-list',),
+    url(r'^sicpe/list/student-in-taught-subject/$', coordination.views.ListStudentInTaughtSubjectView.as_view(), name='student-in-taught-subjects-list',),
 
     url(r'^sicpe/edit/career/(?P<pk>\d+)/$', coordination.views.UpdateCareerView.as_view(), name='careers-edit',),
     url(r'^sicpe/edit/classroom/(?P<pk>\d+)/$', coordination.views.UpdateClassroomView.as_view(), name='classrooms-edit',),
     url(r'^sicpe/edit/generation/(?P<pk>\d+)/$', coordination.views.UpdateGenerationView.as_view(), name='generations-edit',),
     url(r'^sicpe/edit/professor/(?P<pk>\d+)/$', coordination.views.UpdateProfessorView.as_view(), name='professors-edit',),
     url(r'^sicpe/edit/student/(?P<pk>\d+)/$', coordination.views.UpdateStudentView.as_view(), name='students-edit',),
+    url(r'^sicpe/edit/student-in-taught-subject/(?P<pk>\d+)/$', coordination.views.UpdateStudentInTaughtSubjectView.as_view(), name='student-in-taught-subjects-edit',),
 ]
